8409f989b92b31285c1a2e435b6425fbfe6d765a
Mateo <mgomezossa@gmail.com>
2020-04-13 20:29:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
+  var str = '';
   for(let i=1;i<=50;i++){
-    i % 2 === 0 ? x =' Soy Par!' : x = ' Soy Impar!'
-    res.send('<p>'+i+x+'</>');
+    i % 2 === 0 ? x =' Soy Par!' : x = ' Soy Impar!';
+    str = str + '<p>'+i+x+'</>';
   }
+  res.send(str);
 });
 app.listen(3000, () => console.log('Listening on port 3000!'));
